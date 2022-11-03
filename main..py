dbdac32dc037bdfd961d65c5c8010dc1c74f9317
Alexander Melnikov <Msan90546@mail.ru>
2022-11-03 14:38:59 +0300

    Add Random Color

--- main..py
+++ main..py
@@ -3,13 +3,13 @@ from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QColor
 import sys
 from random import randint
-from PyQt5 import uic
+from UI import Ui_Form
 
 
-class Main_Window(QWidget):
+class MainWindow(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
 
         self.draw_btn.clicked.connect(self.run)
 
@@ -23,15 +23,16 @@ class Main_Window(QWidget):
         self.repaint()
 
     def draw_circles(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         radius = randint(1, 200)
         radius2 = randint(1, 200)
         qp.drawEllipse(QPoint(250, 300), radius, radius)
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         qp.drawEllipse(QPoint(650, 300), radius2, radius2)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    my_widget = Main_Window()
+    my_widget = MainWindow()
     my_widget.show()
     sys.exit(app.exec())
