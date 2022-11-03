841d854c5ee15d749d6c4aef63666dde1c0107c8
Alexander Melnikov <Msan90546@mail.ru>
2022-11-03 14:22:41 +0300

    Add Drawing

--- main..py
+++ main..py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
+from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import QPainter, QColor
 import sys
 from random import randint
 from UI import Ui_Form
@@ -12,8 +14,22 @@ class Main_Window(QWidget, Ui_Form):
 
         self.draw_btn.clicked.connect(self.run)
 
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_circles(qp)
+        qp.end()
+
     def run(self):
-        pass
+        self.repaint()
+
+    def draw_circles(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        radius = randint(1, 200)
+        radius2 = randint(1, 200)
+        qp.drawEllipse(QPoint(250, 300), radius, radius)
+        qp.drawEllipse(QPoint(650, 300), radius2, radius2)
+
 
 
 if __name__ == '__main__':
